65fbea05d1001d875f565afead8fed3b8b93ee09
hskim <rhgustmfrh@naver.com>
2022-11-21 18:09:16 +0900

    11.21 upt

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,6 +1,9 @@
 const gnbWrap = document.querySelector(".gnbWrap");
 const depthbg = document.querySelector(".depthbg")
 const depths = document.querySelectorAll(".depth");
+const depthWrap2 = document.querySelectorAll(".depthWrap2");
+const depth2 = document.querySelectorAll(".depth2"); 
+const hbgMenu = document.querySelector(".hbgMenu");
 
 gnbWrap.addEventListener("mouseenter",function(){
     depthbg.style.height = "250px";
@@ -14,4 +17,37 @@ gnbWrap.addEventListener("mouseleave",function(){
     depths.forEach(function(item,index){
         item.style.height = "0px";
     });
+});
+
+//hbg드롭 다운 메뉴 등장
+depthWrap2.forEach(function(item,index){
+    item.addEventListener("click",function(){
+        if(depth2[index].classList.contains("on")){
+            depth2[index].classList.remove("on");
+            item.classList.remove("on");
+        }
+        else{
+            depth2.forEach(function(item,index){
+                item.classList.remove("on");
+                depthWrap2[index].classList.remove("on");
+            });
+            depth2[index].classList.add("on");
+            item.classList.add("on");
+        }
+    });
+});
+
+//햄버거 메뉴
+const hbgBtn = document.querySelector(".hbgBtn");
+const xBtn = document.querySelector(".xBtn");
+const operX = document.querySelector(".operci");
+
+hbgBtn.addEventListener("click",function(){
+    hbgMenu.classList.add("on");
+});
+xBtn.addEventListener("click",function(){
+    hbgMenu.classList.remove("on");
+});
+operX.addEventListener("click",function(){
+    hbgMenu.classList.remove("on");
 });
